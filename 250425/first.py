0b043e43f5a6e61a213579b7906da7bdad2d6c45
kgy2610 <kgy261012@gmail.com>
2025-04-25 17:36:40 +0900

    두 번째 커밋

---
+++ 250425/first.py
@@ -0,0 +1,47 @@
+# 한 줄 주석
+
+'''
+# 이것은 다중 주석석
+'''
+
+print("출력")
+print("출력2")
+
+for i in range(5,0,-1):
+    print("☆ "*i)
+
+# 변수
+
+# 변수이름 = 값
+
+name = "김치찌개"
+name = "김치볶음밥"
+
+name = "김치찌개"
+print(name)
+name = "김치볶음밥"
+print(name)
+
+# 변수 이름은 의미 있어야한다.
+
+name = "김치찌개"
+print(name)
+name ="김치볶음밥"
+print(name)
+
+Test = 20
+
+te_st = 20
+
+# 1test (x)
+# te st = 1 (x)
+
+print(name, name) # 한 줄 출력 변수와 변수 사이는 스페이스
+
+# ex) 10 + 10 = 20 출력
+a = 10
+print(a, " + ", a, " = ",a+a)
+
+PI = 3.14 # 대문자로 이루어진 변수는 상수로 취급
+PI = 2 # BUT, 파이썬은 변수만 허용하기 때문에 아래서 값이 바뀔 수 있음.
+
